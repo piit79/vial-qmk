81d52ffc0bcb3dc4d2442d8d676ccdc630c75c9f
Petr Sedlacek <petr@sedlacek.biz>
2023-06-28 10:33:17 +0200

    [Keyboard] Add 42. Keebs Rev57LP

---
+++ keyboards/42keebs/rev57lp/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF9, 0x6E, 0x02, 0xF6, 0x92, 0x4B, 0x6E, 0x96}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 11}
+
+// Configuration to reduce progmem/flash use
+// #define DYNAMIC_KEYMAP_LAYER_COUNT 2
+// #define VIAL_COMBO_ENTRIES 4
+// #define VIAL_KEY_OVERRIDE_ENTRIES 4
+
+// Extra configuration for the Frood - double tap to reset indicator LED
+// #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED D5
