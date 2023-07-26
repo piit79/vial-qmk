dc3787d3cd536b9fc19039b19572aab2b93cfb44
Petr Sedlacek <petr@sedlacek.biz>
2023-07-26 12:31:49 +0200

    [Keyboard] Add 42. Keebs version of Southpaw75

---
+++ keyboards/42keebs/southpaw75/keymaps/vial/rules.mk
@@ -0,0 +1,10 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+### Uncomment these to save program memory/EEPROM
+# LTO_ENABLE = yes
+# CONSOLE_ENABLE = no
+# QMK_SETTINGS = no
+# TAP_DANCE_ENABLE = no
+# COMBO_ENABLE = no
+# KEY_OVERRIDE_ENABLE = no
