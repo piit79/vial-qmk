ffb1b16b600185317e836b7c0510d28328976f98
Petr Sedlacek <petr@sedlacek.biz>
2023-07-29 13:12:00 +0200

    WIP: add Bongo cat animation for Mysterium

--- keyboards/42keebs/mysterium/config.h
+++ keyboards/42keebs/mysterium/config.h
@@ -28,16 +28,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MATRIX_ROW_PINS { C2, C3, C7, C4, C6, C5 }
 #define MATRIX_COL_PINS { A0, B0, A1, B1, A2, B2, A3, B3, A4, B4, A5, A6, A7, D6, D5, D1, D0, D7 }
-#define UNUSED_PINS
 
 #define DIODE_DIRECTION COL2ROW
 
 #define DEBOUNCE 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+// #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+// #define LOCKING_RESYNC_ENABLE
 
 /* Bootmagic Lite key configuration */
 #define BOOTMAGIC_LITE_ROW 0
