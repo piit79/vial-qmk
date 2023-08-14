42fd0c0fe6ed1961102cbd344f14a0ea6af7ad92
Petr Sedlacek <petr@sedlacek.biz>
2023-08-14 08:33:42 +0200

    [Keyboard] Add 42. Keebs OmegaISO

---
+++ keyboards/42keebs/omegaiso/README.md
@@ -0,0 +1,3 @@
+# OmegaISO
+
+A meme board with a TKL layout composed entirely of ISO keys. Because why not :)
---
+++ keyboards/42keebs/omegaiso/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2D, 0xA4, 0x58, 0x6C, 0xE8, 0x04, 0x45, 0x02}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 9}
+#define VIAL_UNLOCK_COMBO_COLS {0, 4}
