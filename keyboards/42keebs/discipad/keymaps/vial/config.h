e2744ebc7bf19a98477a24cddc4f7962a37112f7
Petr Sedlacek <petr@sedlacek.biz>
2023-12-18 16:11:45 +0100

    [Keyboard] Add 42. Keebs version of Discipad

---
+++ keyboards/42keebs/discipad/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x00, 0x55, 0x64, 0x26, 0x1C, 0x51, 0xBA, 0xD9}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
