79cfdd5873b30171ab6793f7b8375eebc87c73db
Petr Sedlacek <petr@sedlacek.biz>
2023-12-22 09:01:30 +0100

    [Keyboard] Add 42. Keebs Mysteripad

---
+++ keyboards/42keebs/mysteripad/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3D, 0x0F, 0xBC, 0x59, 0x71, 0xDB, 0x17, 0xC8}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
