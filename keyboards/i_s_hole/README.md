926fff54b51707a97285669ee6b9673a0d91dbaa
Petr Sedlacek <petr@sedlacek.biz>
2023-12-26 14:19:48 +0100

    [Keyboard] Add I-S-hOle

---
+++ keyboards/i_s_hole/README.md
@@ -0,0 +1,3 @@
+# I-S-hOle
+
+Vial firmware for the [I-S-hOle](https://www.40percent.club/2018/11/i-s-hole.html) 4 ISO Enter key macropad
---
+++ keyboards/i_s_hole/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID {0x13, 0xFF, 0xC8, 0x47, 0xEC, 0xEE, 0x4A, 0xBE}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
