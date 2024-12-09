ad8a195823efc07600632feb5ee50ea71a2cc100
Petr Sedlacek <petr@sedlacek.biz>
2024-12-09 14:45:22 +0100

    [Keyboard] Add 42. Keebs pISOlation

---
+++ keyboards/42keebs/pisolation/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2024 piit79
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE8, 0x3A, 0x3D, 0x2E, 0x70, 0x3D, 0x64, 0x1C}
+#define VIAL_UNLOCK_COMBO_ROWS { 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 1
