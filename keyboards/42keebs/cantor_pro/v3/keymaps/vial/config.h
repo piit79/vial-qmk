78e06e1f5f56381bbd72c314d535b947b348bb65
Petr Sedlacek <petr@sedlacek.biz>
2025-10-21 21:17:10 +0200

    [Keyboard] Add support for 42. Keebs Cantor Pro v3

---
+++ keyboards/42keebs/cantor_pro/v3/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* Keyboard uid */
+#define VIAL_KEYBOARD_UID {0xA1, 0x28, 0x9D, 0xBF, 0x43, 0x00, 0xC3, 0x08}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0 }
+
+/* Default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
